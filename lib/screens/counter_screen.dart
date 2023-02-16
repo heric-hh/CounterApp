ddc5e529bcf01b69fd84bd09baabae0e4bee66ff
heric-hh <erhh.015@gmail.com>
2023-02-16 11:58:04 -0600

    Refactorizando

--- lib/screens/counter_screen.dart
+++ lib/screens/counter_screen.dart
@@ -9,6 +9,22 @@ class CounterScreen extends StatefulWidget {
 
 class _CounterScreenState extends State<CounterScreen> {
   int contador = 0;
+
+  void incrementar() {
+    contador++;
+    setState(() {});
+  }
+
+  void decrementar() {
+    contador--;
+    setState(() {});
+  }
+
+  void reiniciar() {
+    contador = 0;
+    setState(() {});
+  }
+
   final TextStyle estiloTexto =
       const TextStyle(fontSize: 30, color: Colors.black54, fontFamily: 'Lato');
 
@@ -43,51 +59,57 @@ class _CounterScreenState extends State<CounterScreen> {
           ],
         ),
       ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          const SizedBox(
-            width: 15,
-          ),
-          FloatingActionButton.extended(
-            onPressed: () => setState(() {
-              contador++;
-            }),
-            label: const Text('Súmale Rey'),
-            icon: const Icon(Icons.add),
-            foregroundColor: Colors.black,
-            backgroundColor: const Color.fromRGBO(224, 247, 116, 1),
-          ),
-          FloatingActionButton.extended(
-            onPressed: () => setState(() {
-              contador = 0;
-            }),
-            label: const Text('😒'),
-            icon: const Icon(Icons.restore),
-            foregroundColor: Colors.black,
-            backgroundColor: const Color.fromRGBO(224, 247, 116, 1),
-          ),
-          FloatingActionButton.extended(
-            onPressed: () => setState(() {
-              contador--;
-            }),
-            label: const Text('Quítale'),
-            icon: const Icon(Icons.exposure_minus_1),
-            foregroundColor: Colors.black,
-            backgroundColor: const Color.fromRGBO(224, 247, 116, 1),
-          ),
-        ],
+      floatingActionButton: CustomFloatingActionButton(
+        decrease: decrementar,
+        increase: incrementar,
+        reset: reiniciar,
       ),
-      /*floatingActionButton: FloatingActionButton(
-        foregroundColor: Colors.black,
-        backgroundColor: const Color.fromRGBO(224, 247, 116, 1),
-        child: const Icon(Icons.add),
-        onPressed: () {
-          contador++;
-          print('Presionaste el boton');
-          print(contador);
-        },
-      ),*/
+    );
+  }
+}
+
+class CustomFloatingActionButton extends StatelessWidget {
+  final Function increase;
+  final Function decrease;
+  final Function reset;
+
+  const CustomFloatingActionButton({
+    Key? key,
+    required this.increase,
+    required this.decrease,
+    required this.reset,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        const SizedBox(
+          width: 15,
+        ),
+        FloatingActionButton.extended(
+          onPressed: () => increase(),
+          label: const Text('Súmale Rey'),
+          icon: const Icon(Icons.add),
+          foregroundColor: Colors.black,
+          backgroundColor: const Color.fromRGBO(224, 247, 116, 1),
+        ),
+        FloatingActionButton.extended(
+          onPressed: () => reset(),
+          label: const Text('😒'),
+          icon: const Icon(Icons.restore),
+          foregroundColor: Colors.black,
+          backgroundColor: const Color.fromRGBO(224, 247, 116, 1),
+        ),
+        FloatingActionButton.extended(
+          onPressed: () => decrease(),
+          label: const Text('Quítale'),
+          icon: const Icon(Icons.exposure_minus_1),
+          foregroundColor: Colors.black,
+          backgroundColor: const Color.fromRGBO(224, 247, 116, 1),
+        ),
+      ],
     );
   }
 }
